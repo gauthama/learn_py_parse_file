dd8688b75a7a723889eb5b5b9038dd2f303fc459
Gauthama Periasamy <gauthama.periasamy@sysenso.com>
2026-02-20 10:36:32 +0530

    Uses streaming

--- parse_m1d.py
+++ parse_m1d.py
@@ -1,26 +1,69 @@
+# Simplified M1D data file content:
+# # Table format: T1D
+# # table_unit = null
+# # axis1_unit = null
+# # axis2_unit = null
+# 0.0     3
+# 2.3     1.2
+# 7.5     0.5
+# 1.5     1.0
+# 5.0     2
+# 0.0     3.4
+# 6.9     5.1
 
+
+
+#Input:
+# 3
+# 21    9
+# 7     5
+# 8     1
+# 2
+# 0     3
+# 6     5
+
+#Output:
+# {3: [[21, 7, 8],[9, 5, 1]],
+# 2: [[0, 6],[3, 5]]}
 
 def parse_m1d_data_file(file_path):
-    result = [[], []]
+    result = {}
 
     with open(file_path, "r", encoding="utf-8") as f:
-        for line in f:
-            line = line.strip()
-            # skip empty lines and comments
-            if not line.strip() or line.startswith("#"):
-                continue
+        while True:
+            # Read block size (skip comments/empty lines)
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith("#"):
+                    tmp_list =line.split()
+                    slice_value = tmp_list[0]
+                    count = int(float(tmp_list[1]))
+                    break
+            else:
+                break  # EOF reached
 
-            a, b = map(float, line.split())
-            result[0].append(a)
-            result[1].append(b)
+            col1 = []
+            col2 = []
 
-    print(result)
+            for _ in range(count):
+                for line in f:
+                    line = line.strip()
+                    if line and not line.startswith("#"):
+                        a, b = map(float, line.split())
+                        col1.append(a)
+                        col2.append(b)
+                        break
+                else:
+                    raise ValueError("Unexpected EOF inside data block")
+
+            result[slice_value] = [col1, col2]
+
+    #print(result)
     return result
 
-
-
+   
 # Usage
 if __name__ == "__main__":
     file_path = "sample_formats/data/M1D_tiny.data"
     output = parse_m1d_data_file(file_path)
-    print(output)  # [[0, 7, 1], [2, 5, 3]]
+    print(output)
