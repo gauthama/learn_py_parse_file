76bc1d35b583f1ed85242b03482bb5acba9a6b65
Gauthama Periasamy <gauthama.p@gmail.com>
2026-02-24 18:43:11 +0530

    added def reverse_dimension_units

--- parse_header.py
+++ parse_header.py
@@ -12,18 +12,18 @@
 from parse_data_file import extract_dim_from_header
 import re
 
-def extract_units_using_regular_expressions(file_path):
-    #import re
+# def extract_units_using_regular_expressions(file_path):
+#     #import re
 
-    units = {}
+#     units = {}
 
-    pattern = re.compile(r"#\s*axis(\d+)_unit\s*=\s*(.+)")
+#     pattern = re.compile(r"#\s*axis(\d+)_unit\s*=\s*(.+)")
 
-    with open("data.txt") as f:
-        for line in f:
-            m = pattern.match(line)
-            if m:
-                units[m.group(1)] = m.group(2)
+#     with open("data.txt") as f:
+#         for line in f:
+#             m = pattern.match(line)
+#             if m:
+#                 units[m.group(1)] = m.group(2)
 
 def get_dimension_from_data_file(file_path):
     first_line = open(file_path, "r", encoding="utf-8").readline().rstrip("\n")
@@ -31,6 +31,23 @@ def get_dimension_from_data_file(file_path):
     suffix = "D"
     dimension = extract_dim_from_header(first_line, prefix, suffix)
     return dimension
+
+# Input: {'1': 'degC', '2': '%', '3': 'Nm'}
+# Output: {'D1': 'Nm', 'D2': '%', 'D3': 'degC'}
+def reverse_dimension_units(units_dict):
+    # sort keys numerically
+    keys = sorted(units_dict.keys(), key=int)
+
+    # reverse values according to numeric key order
+    reversed_values = [units_dict[k] for k in keys[::-1]]
+
+    # build result dictionary
+    result = {}
+    for index, value in enumerate(keys):
+        result[f"D{value}"] = reversed_values[index]
+
+    #print(result)
+    return result
 
 def parse_header_of_data_file(file_path):
     dimension = get_dimension_from_data_file(file_path)
@@ -66,14 +83,18 @@ def parse_header_of_data_file(file_path):
                     units[m.group(1)] = m.group(2)
             else:
                 break  # stop reading after the header section
-    return dimension,units
+
+    print(f"Extracted units dictionary: {units}")
+    reversed_units = reverse_dimension_units(units)
+    print(f"Reversed units dictionary: {reversed_units}")
+    return dimension,reversed_units
 
 
 # Usage
 if __name__ == "__main__":
     file_path1 = "sample_formats/data/3D.data"
     output = parse_header_of_data_file(file_path1)
-    print(output)
+    #print(output)
     file_path2 = "sample_formats/data/2D.data"
     output = parse_header_of_data_file(file_path2)
-    print(output)
+    #print(output)
